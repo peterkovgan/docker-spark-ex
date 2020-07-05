2efdfad5e34e3eb456da3d0cc43f1fd9dd103065
peter <peter@localhost.localdomain>
2020-07-05 13:20:13 +0300

    scala ex

---
+++ Dockerfile
@@ -0,0 +1,7 @@
+FROM bde2020/spark-scala-template:3.0.0-hadoop3.2
+
+
+MAINTAINER Peter <peter.kovgan@gmail.com>
+
+
+
---
+++ src/main/scala/Application.scala
@@ -0,0 +1,33 @@
+import org.apache.spark.SparkContext
+import org.apache.spark.SparkContext._
+import org.apache.spark.SparkConf
+
+
+object Application{
+
+   val NUM_SAMPLES = 25
+
+
+   def main(args:Array[String]):Unit={
+
+        println("Hello, Ganesh!")
+
+        val sc = new SparkContext(new SparkConf().setAppName("Computation"))
+
+        val count = sc.parallelize(1 to NUM_SAMPLES).filter { _ =>
+
+             val x = math.random
+
+             val y = math.random
+
+             x*x + y*y < 1
+
+        }.count()
+
+        println(s"Pi is roughly ${4.0 * count / NUM_SAMPLES}")
+
+        
+        //sc.stop
+   }
+
+}
